f78580b284ea8210f07036e7e266b860d36b97f5
aaronquansrx <94507687+aaronquansrx@users.noreply.github.com>
2026-06-01 13:22:34 +1000

    AST 2

--- ts_to_cpp/main.py
+++ ts_to_cpp/main.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import sys
 from dataclasses import dataclass
 from enum import Enum
@@ -9,7 +10,7 @@ ts_type_to_ctype = {
   "boolean": "bool"
 }
   
-def insideBrackets(s: str) -> str:
+def insideBrackets(s: str) -> int:
   open_b = 0
   quote = None
   for i in range(len(s)):
@@ -34,27 +35,70 @@ class NodeType(Enum):
   VAR = 5
   CLASS = 6
 
-def getTypeOfLine(s: str) -> NodeType:
-  sp = s.lstrip().split(" ") # split on special character
-  first = sp[0]
+
+operators = ["+", "-"]
+
+def firstWord(line: str, i: int=0) -> tuple[str, int]:
+  sp_chars = [" ", "=", "("]
+  st = i
+  #i = 0
+  while i < len(line):
+    if any(line[i] == c for c in sp_chars):
+        break
+    i+=1
+  return line[st:i], i
+
+def lwhiteSpaceIndex(line: str) -> int:
+  i = 0
+  sp = [' ', '\n', '\t']
+  while i < len(line):
+    if all(line[i] != c for c in sp):
+      break
+    i+=1
+  return i
+
+def getTypeOfLine(line: str) -> tuple[NodeType, int]:
+  #sp = s.lstrip().split(" ") # split on special character
+  i = lwhiteSpaceIndex(line)
+  first, i = firstWord(line, i)
+  typ = NodeType.FUNCTION
   print(first)
   if first == "function":
-    return NodeType.FUNCTION
+    typ = NodeType.FUNCTION
   elif first == "for":
-    return NodeType.FOR
+    typ = NodeType.FOR
   elif first == "if":
-    return NodeType.IF
+    typ = NodeType.IF
   elif first == "return":
-    return NodeType.RETURN
+    typ = NodeType.RETURN
   elif first == "var" or first == "const" or first == "let":
-    return NodeType.VAR
-
+    typ = NodeType.VAR
+  return typ, i
 
 class ASTNode:
   def __init__(self):
     self.type = NodeType.FUNCTION
     self.name = ""
     self.children = [] # ASTNode[]
+
+@dataclass
+class OperatorNode:
+  operator: str
+  value_node: ValueNode
+
+@dataclass
+class ValueNode:
+  value: ValueNode | str
+  next: OperatorNode | None
+
+@dataclass
+class VariableAssignmentNode:
+  name: str
+  value_node: ValueNode 
+
+
+def parseVariableAssignmentLine(line: str) -> VariableAssignmentNode:
+  pass
 
 def parseFunctionHeader(line: str) -> ASTNode:
   pass
@@ -81,9 +125,11 @@ def main():
     try:
       with open(fn) as f:
         content = f.read()
-        print(content)
         for line in content.splitlines():
-          getTypeOfLine(line)
+          tp, i = getTypeOfLine(line)
+          rest = line[i:].lstrip()
+          if tp == NodeType.VAR:
+            print(rest)
     except FileNotFoundError:
       print("not found")
   pass
